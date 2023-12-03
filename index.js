f8367501b4dab8a514496870f2168ad22ce103d5
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-12-03 15:28:20 +0530

    testing multer file upload

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", multer.single("avatar"), function (req, res) {
+  console.log(req);
+  console.log(req.body);
+  console.log(req.file);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });

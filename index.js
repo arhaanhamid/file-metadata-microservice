d9149fe37ed0c449dbb045dc7f85965c326e9a15
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-12-03 16:42:00 +0530

    testing multer file upload t20

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const multer = require("multer");
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/uploads");
+    cb(null, "/tmp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
